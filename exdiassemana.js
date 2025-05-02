7e17bd22291f0c2f3bac3be5a3b1d14a84abfa43
Callax <pedro.ferreira_farinha@outlook.pt>
2025-05-02 15:52:06 +0100

    ex js 2.1

---
+++ exdiassemana.js
@@ -0,0 +1,35 @@
+let num=5;
+
+switch(num){
+case 1:
+console.log("Sun");
+break;
+
+case 2:
+console.log("Mon");
+break;
+
+case 3:
+console.log("Tue");
+break;
+
+case 4:
+console.log("Wed");
+break;
+
+case 5:
+console.log("Thrs");
+break;
+
+case 6:
+console.log("Fri");
+break;
+
+case 7:
+console.log("Sat");
+break;
+
+default:
+console.log("Error");
+break;
+}
---
+++ exmedias.js
@@ -0,0 +1,13 @@
+let medg;
+let medk;
+
+medg=(96+108+89)/3;
+medk=(88+91+110)/3;
+
+if(medg > medk){
+    console.log("Os Golfinhos ganham!");
+}else if (medg < medk){
+    console.log("Os Koalas Vencem!");
+}else{
+    console.log("Empate! \n As equipas devem escolher um campeão para lutar até a morte!");
+}
