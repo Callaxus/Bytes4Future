e052b2ebc8fb19f44037c629f6d002b6186919d2
Callax <pedro.ferreira_farinha@outlook.pt>
2025-05-02 20:10:36 +0100

    correção ex randnumber alert still goofs

--- ex_random_number.js
+++ ex_random_number.js
@@ -1,24 +1,21 @@
-/* 
-Roll a number between 1-10
-ask usr for another number
-while numbers are diferent keep asking usr for numbers
-if numbers equal usr wins
-*/
 
-
-let unum = prompt("Insira um numero entre 1 e 10");
+let unum ; 
 let rnum = Math.floor(Math.random() * 10) + 1;
 
+unum = parseInt(prompt("Insira um numero entre 1 e 10"));
 
-while(unum != rnum){
-    unum = prompt("Guess the number!\nInsira um numero entre 1 e 10");
-    rnum = Math.floor(Math.random() * 10) + 1;
-    console.log(`${unum} é o numero errado! \nO numero certo era: ${rnum}`);
-    if(unum<=0 || unum >10 ){
-        console.log("Numero tem de ser maior que 0 e menor que 10!");
+if (isNaN(unum) || unum < 1 || unum > 10){
+    alert("Valor fora dos Parametros! \n Insira um numero entre 1 e 10");
+}else if(unum === rnum){
+    alert(`Parabéns Shinji!\n RanNumber: ${rnum} O seu Numero: ${unum}` );
+}else{
+    while(unum !== rnum){
+        alert(`Try Again\n RanNumber: ${rnum} O seu Numero: ${unum}`);
+        rnum = Math.floor(Math.random() * 10) + 1;
+        unum = parseInt(prompt("Insira um numero entre 1 e 10"));
+
+        if (isNaN(unum) || unum < 1 || unum > 10){
+            alert("Valor fora dos Parametros! \n Insira um numero entre 1 e 10");
+        }
     }
-}
-
-{
-    console.log(` random number is:${rnum} \n${unum} é o numero certo! \n Parabéns Shinji!`);
 }
