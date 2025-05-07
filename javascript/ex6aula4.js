0df2d81e8622762e32b1985084f1ee655774715c
callaxus <pedro.ferreira_farinha@outlook.pt>
2025-05-07 17:37:15 +0100

    123456789

---
+++ javascript/ex6aula4.js
@@ -0,0 +1,6 @@
+const pers ={
+    nome: "João",
+    idade: "30",
+};
+
+console.log(`Nome: ${pers.nome} \nIdade: ${pers.idade}`)
---
+++ javascript/ex7aula4.js
@@ -0,0 +1,16 @@
+const car ={
+    brand:"Ford",
+    model:"Ka +",
+    year:"2018",
+    color:"Oxford White"
+};
+
+console.log(car);
+
+car.color = "Blue";
+
+console.log(car);
+
+car.odometer = "20000 Km";
+
+console.log(car);
