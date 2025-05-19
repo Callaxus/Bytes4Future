78e04a9280a59f9e804427d2809d801831a9ca92
Callax <pedro.ferreira_farinha@outlook.pt>
2025-05-19 16:48:09 +0100

    aula fim

---
+++ AVANÇADO/JavaScript/ex1nomeidade.js
@@ -0,0 +1,7 @@
+function apresntaNomeIdade(nome,idade){
+    let frase=`O meu nome é ${nome} e tenho ${idade} anos.`
+    return frase;
+}
+let ii="Carolina Olivia"
+let nn=29;
+console.log(apresntaNomeIdade(ii,nn));
