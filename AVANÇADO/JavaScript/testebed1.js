e623d2f429fe488775b28cf79ddab1d88abc444c
callaxus <pedro.ferreira_farinha@outlook.pt>
2025-05-20 18:28:18 +0100

    1111

--- AVANÇADO/JavaScript/testebed1.js
+++ AVANÇADO/JavaScript/testebed1.js
@@ -1,7 +1,24 @@
-function retornaNomeEIdade(nome, idade) {
-return`O meu nome é ${nome}e tenho ${idade} anos.`
+function calculaFaixaEtaria(idade){
+    let faixa;
+    if(idade>0||idade<=11){
+        faixa="Criança";
+        return faixa;
+    }else if(idade>=12||idade<=17){
+        faixa="Adolescente";
+        return faixa;
+    }else if(idade>=18||idade<=64){
+        faixa="Adulto";
+        return faixa;                
+    }else if(idade>=65){
+        faixa="Sénior";
+        return faixa;            
+    }else{
+        return;
+    }            
 }
 
-console.log(retornaNomeEIdade("Carolina",50));
-
-
+console.log(calculaFaixaEtaria(1));
+console.log(calculaFaixaEtaria(15));
+console.log(calculaFaixaEtaria(25));
+console.log(calculaFaixaEtaria(65));
+console.log(calculaFaixaEtaria(-1));
