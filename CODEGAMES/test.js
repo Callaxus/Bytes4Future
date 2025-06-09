126ee4b057ba1957306137744583b7b46247b37a
Callax <pedro.ferreira_farinha@outlook.pt>
2025-06-09 08:25:34 +0100

    cenas tic tac toes

--- CODEGAMES/test.js
+++ CODEGAMES/test.js
@@ -1,8 +1,32 @@
-function verificaPangrama(string) {
-    const todas = "abcdefghijlmnopqrstuvxz"
-    const minusculas = string.toLowerCase()
-
-    return minusculas.includes(todas.all);
+class Pessoa {
+    constructor(nome, idade, gender){
+        this.nome = nome;
+        this.idade = idade;
+        this.sexo = sexo;
     }
 
-    console.log(verificaPangrama(" pequeno jabuti xareta viu dez cegonhas felizes "))
+    comparaIdade(pess){
+        
+        if(this.idade < pess.idade && pess.sexo === "m") {
+            return `O ${pess.nome} é mais velho do que eu.`;
+        }
+        else if(this.idade > pess.idade && pess.sexo === "m" ){
+            return `O ${pess.nome} é mais novo do que eu.`
+        }
+        else if(this.idade === pess.idade && pess.sexo === "m"){
+            return `O ${pess.nome} é da mesma idade que eu.`
+        }
+        else if(this.idade < pess.idade && pess.sexo === "f"){
+            return `A ${pess.nome} é mais velha do que eu.`
+        }
+        else if(this.idade > pess.idade && pess.sexo === "f"){
+            return `A ${pess.nome} é mais nova do que eu.`
+        }
+        else if(this.idade === pess.idade && pess.sexo === "f"){
+            return `A ${pess.nome} é da mesma idade que eu.`
+        }
+        else{
+            return;
+        }
+    }
+}
